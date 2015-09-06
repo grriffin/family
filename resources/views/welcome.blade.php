e04574d3bb86039781913e0c581630f69bdf25b3
Cam <cam.pope@gmail.com>
2015-09-06 13:37:42 -0600

    style changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
 @section('content')
   <div id="home">
     <div class="row navigation">
-      <div class="col-sm-8">
+      <div class="col-xs-8">
 
       </div>
-      <div class="col-sm-4">
+      <div class="col-xs-4">
         @if (Auth::check())
         <a class="btn btn-primary" href="auth/logout">Logout</a>
         @else
@@ -18,24 +18,25 @@
       </div>
     </div>
     <div class="row">
-      @if (Auth::check())
-        <h2>Sup {{$user->name}}</h2>
-        @if (isset($chats))
-          @foreach ($chats as $chat)
-            <div class="printchat">
-              <p>{{ $chat->name }}</p><p>{{ $chat->message }}</p>
-            </div>
-          @endforeach
+      <div class="col-xs-6 col-xs-offset-3">
+        @if (Auth::check())
+          <h2>Sup {{$user->name}}</h2>
+          @if (isset($chats))
+            @foreach ($chats as $chat)
+              <div class="printchat">
+                <p>{{ $chat->name }}</p><p>{{ $chat->message }}</p>
+              </div>
+            @endforeach
+          @endif
+          <form id="chatform" method="POST" action="">
+            {!! csrf_field() !!}
+            <textarea name="message" form="chatform"></textarea>
+            <input type="submit" name="submit" class="btn btn-success">
+          </form>
+        @else 
+          <p>You are not signed in.</p>
         @endif
-        <form id="chatform" method="POST" action="">
-          {!! csrf_field() !!}
-          <textarea name="message" form="chatform"></textarea>
-          <input type="submit" name="submit" class="btn btn-success">
-        </form>
-      @else 
-        <p>You are not signed in.</p>
-      @endif
-     
+      </div>
     </div>
   </div>
 @endsection
