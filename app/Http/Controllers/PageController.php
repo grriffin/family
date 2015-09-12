5959f517c10f6674b6a9664f91fda63d365c8d08
Cam <cam.pope@gmail.com>
2015-09-11 23:17:03 -0600

    this is it

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -46,7 +46,7 @@ class PageController extends Controller
           Mail::send('emails.message', $data, function ($message) use ($data) {
             $message->from('popechats@campope.com');
 
-            $message->to($user->email);
+            $message->to($data['email']);
 });
         }
         else {
